4559ffe23a5e839e22d566fde4e1b2745abbf9ab
Mehedi0014 <mehedi0014@gmail.com>
2023-07-16 23:42:46 +0600

    add pagination

--- views/mmhs-table-list.php
+++ views/mmhs-table-list.php
@@ -13,10 +13,28 @@ class MmhsTableList extends WP_List_Table {
   public function prepare_items() {
     $orderby = isset( $_GET['orderby'] ) ? trim( $_GET['orderby'] ) : ""; // come form query string
     $order = isset( $_GET['order'] ) ? trim( $_GET['order'] ) : ""; // come form query string
-    $search_term = isset( $_POST['s'] ) ? trim( $_POST['s'] ) : "";       
-    $this->items = $this->wp_list_table_data( $orderby, $order); // Sokol data ja amara display korbo ta ekhane items variable e store kora holo.
+    $search_term = isset( $_POST['s'] ) ? trim( $_POST['s'] ) : "";
 
-    $this->items = $this->wp_list_table_data( $orderby, $order, $search_term); // Sokol data ja amara display korbo ta ekhane items variable e store kora holo.
+    $datas = $this->wp_list_table_data( $orderby, $order, $search_term); // Sokol data ja amara display korbo ta ekhane items variable e store kora holo.
+
+    $per_page = 3;
+    $current_page = $this->get_pagenum();
+    $total_items = count($datas);
+    $this->set_pagination_args(array(
+      "total_items" => $total_items,
+      "per_page"    => $per_page,
+
+    ));
+
+    $this->items = array_slice( $datas, ( ( $current_page - 1 ) * $per_page ), $per_page ); 
+    /* 
+      1st paramitter contain all values, 2nd are start section and 3rd are how many record we want to show. Sokol data ja amara display korbo ta ekhane items variable e store kora holo.
+      ( ( $current_page - 1 ) * $per_page ) === details below
+      1-1 = 0*3 => 0; so 1st page show 0,1 2 index data
+      2-1 = 1*3 => 3; so 2ns page show 3,4,5 index data
+      3-1 = 2*3 => 6; so 3ns page show 6,7,8 index data
+    */
+
     $columns = $this->get_columns(); // Ai function dara j j kolam lagbe tar process kora hoy.
     $hidden = $this->get_hidden_columns(); // use to hide any coloumn. get_columns method e j column key diclare korechi just sei column key pass kore detay hobe: jemon name ba email.
     $sortable = $this->get_sortable_columns(); // table k shortable korar jonno use koar hoy. url er sese query string pass kore kaj ta kora hoy like: orderby=name&order=asc
